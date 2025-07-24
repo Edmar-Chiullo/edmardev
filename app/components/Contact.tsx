813cec6dafcb80eaad45292507410340b8c338df
Edmar-Chiullo <ecschiullo@gmail.com>
2025-07-24 01:51:09 -0300

    Ajuste do box da sessão contato

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -4,8 +4,8 @@ import { Element } from "react-scroll";
 export default function Contact() {
   return (
     <Element name="contact">
-      <section id="contact" className="flex flex-col gap-10 w-full h-screen py-10  px-6 bg-blue-50 text-center">
-        <h2 className="text-3xl font-bold lg:text-2xl text-blue-700/70">Vamos conversar?</h2>
+      <section id="contact" className="flex flex-col gap-10 w-full h-screen lg:py-32 px-6 bg-blue-50 text-center">
+        <h2 className="text-3xl font-bold lg:text-2xl text-blue-700/80">Vamos conversar?</h2>
         <p className="text-lg">Entre em contato para tirar dúvidas ou solicitar um orçamento.</p>
         <a
           href="mailto:seuemail@gmail.com"
